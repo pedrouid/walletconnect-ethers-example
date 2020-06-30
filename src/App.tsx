4a72d8e9f5a512e3b1e9dbd0067340bf1a347eac
Pedro Gomes <pedrogomes94@gmail.com>
2020-06-30 21:10:52 +0200

    improve actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import WalletConnectProvider from "@walletconnect/web3-provider";
-import { providers, utils } from "ethers";
+import { Contract, providers, utils } from "ethers";
 
 // @ts-ignore
 import logo from "./logo.svg";
 import "./App.css";
 
+const DAI = {
+  address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+  abi: [
+    "function transfer(address _to, uint256 _value) returns (bool success)",
+  ],
+};
+
 function App() {
-  const [address, setAccounts] = React.useState<string>("");
+  const [address, setAddress] = React.useState<string>("");
   const [provider, setProvider] = React.useState<providers.Web3Provider>();
+
+  function reset() {
+    console.log("reset");
+    setAddress("");
+    setProvider(undefined);
+  }
 
   async function connect() {
     if (!process.env.REACT_APP_INFURA_ID) {
@@ -18,30 +31,47 @@ function App() {
       infuraId: process.env.REACT_APP_INFURA_ID,
     });
 
-    const accounts = (await web3Provider.enable()) as string[];
+    web3Provider.on("disconnect", reset);
 
-    setAccounts(accounts[0]);
+    const accounts = (await web3Provider.enable()) as string[];
+    setAddress(accounts[0]);
 
     const provider = new providers.Web3Provider(web3Provider);
-
-    const msg = `Authenticate ${accounts[0]}`;
-
-    const sig = await provider.send("personal_sign", [msg, accounts[0]]);
-
-    console.log("Signature", sig);
-
-    const signer = await utils.verifyMessage(msg, sig);
-
-    console.log("isValid", signer === accounts[0]);
-
     setProvider(provider);
+  }
+
+  async function signMessage() {
+    if (!provider) {
+      throw new Error("Provider not connected");
+    }
+    const msg = `Authenticate ${address}`;
+    const sig = await provider.send("personal_sign", [msg, address]);
+    console.log("Signature", sig);
+    console.log("isValid", utils.verifyMessage(msg, sig) === address);
+  }
+
+  async function transferDai() {
+    if (!provider) {
+      throw new Error("Provider not connected");
+    }
+    const contract = new Contract(DAI.address, DAI.abi, provider);
+    const res = await contract.transfer(address, utils.parseEther("1"));
+    console.log("res", res);
   }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>{provider ? "Connected!" : "Not connected"}</div>
-        {!address ? <div onClick={connect}>Connect</div> : <div>{address}</div>}
+        {address ? (
+          <>
+            <div>{address}</div>
+            <button onClick={signMessage}>Sign Message</button>
+            <button onClick={transferDai}>Transfer DAI</button>
+          </>
+        ) : (
+          <button onClick={connect}>Connect</button>
+        )}
       </header>
     </div>
   );
