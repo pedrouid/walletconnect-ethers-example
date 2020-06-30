0600ed2624c1c1d2301fedee3cd02e528a152c0b
Pedro Gomes <pedrogomes94@gmail.com>
2020-06-30 20:32:35 +0200

    add personal_sign

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import WalletConnectProvider from "@walletconnect/web3-provider";
-import { providers } from "ethers";
+import { providers, utils } from "ethers";
 
 // @ts-ignore
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [provider, setprovider] = React.useState<providers.Web3Provider>();
+  const [address, setAccounts] = React.useState<string>("");
+  const [provider, setProvider] = React.useState<providers.Web3Provider>();
 
   async function connect() {
     if (!process.env.REACT_APP_INFURA_ID) {
@@ -17,18 +18,30 @@ function App() {
       infuraId: process.env.REACT_APP_INFURA_ID,
     });
 
-    await web3Provider.enable();
+    const accounts = (await web3Provider.enable()) as string[];
+
+    setAccounts(accounts[0]);
 
     const provider = new providers.Web3Provider(web3Provider);
 
-    setprovider(provider);
+    const msg = `Authenticate ${accounts[0]}`;
+
+    const sig = await provider.send("personal_sign", [msg, accounts[0]]);
+
+    console.log("Signature", sig);
+
+    const signer = await utils.verifyMessage(msg, sig);
+
+    console.log("isValid", signer === accounts[0]);
+
+    setProvider(provider);
   }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>{provider ? "Connected!" : "Not connected"}</div>
-        <div onClick={connect}>Connect</div>
+        {!address ? <div onClick={connect}>Connect</div> : <div>{address}</div>}
       </header>
     </div>
   );
