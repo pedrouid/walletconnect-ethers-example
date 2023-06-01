ed7dfbe92bf411058bec211aa42b6734626ec4f2
Pedro Gomes <pedrogomes94@gmail.com>
2023-06-01 10:06:18 +0200

    add switchChain

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,34 @@ import logo from "./logo.svg";
 import "./App.css";
 import { formatAuthMessage } from "./utils";
 
+interface Chain {
+  chainId: string;
+  chainName?: string;
+  rpcUrls?: string[];
+}
+type Chains = Record<number, Chain>;
+
+const ETHEREUM_RPC_URL = `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`;
+const POLYGON_RPC_URL = "https://polygon.llamarpc.com";
+
 const DAI = {
   address: "q",
   abi: [
     "function transfer(address _to, uint256 _value) returns (bool success)",
   ],
+};
+
+const CHAINS: Chains = {
+  1: {
+    chainId: "0x1",
+    chainName: "Ethereum Mainnet",
+    rpcUrls: [ETHEREUM_RPC_URL],
+  },
+  137: {
+    chainId: "0x89",
+    chainName: "Polygon PoS",
+    rpcUrls: [POLYGON_RPC_URL],
+  },
 };
 
 function App() {
@@ -21,6 +44,7 @@ function App() {
 
   function reset() {
     console.log("reset");
+    setChainId(1);
     setAddress("");
     setProvider(undefined);
   }
@@ -38,6 +62,21 @@ function App() {
     const accounts = (await web3Provider.enable()) as string[];
     setAddress(accounts[0]);
     setChainId(web3Provider.chainId);
+
+    web3Provider.on("accountsChanged", (accounts: string[]) => {
+      console.log("accountsChanged", accounts);
+      setAddress(accounts[0]);
+    });
+
+    web3Provider.on("chainChanged", (chainId: number) => {
+      console.log("chainChanged", chainId);
+      setChainId(chainId);
+    });
+
+    web3Provider.on("disconnect", (error: any) => {
+      console.log("disconnect", error);
+      reset();
+    });
 
     const provider = new providers.Web3Provider(web3Provider);
     setProvider(provider);
@@ -62,6 +101,41 @@ function App() {
     console.log("res", res);
   }
 
+  async function switchChain() {
+    if (!provider) {
+      throw new Error("Provider not connected");
+    }
+    let newChainId = chainId === 1 ? 137 : 1;
+    const chain = CHAINS[newChainId];
+    if (!chain) {
+      throw new Error(`Unknown chainId: ${newChainId}`);
+    }
+    try {
+      await provider.send("wallet_switchEthereumChain", [
+        { chainId: chain.chainId },
+      ]);
+    } catch (error) {
+      console.log("error.code", (error as any).code);
+      if ((error as any).code === 4902) {
+        try {
+          await provider.send("wallet_addEthereumChain", [chain]);
+        } catch (error) {
+          // ignore error
+        }
+      } else {
+        // ignore error
+      }
+    }
+  }
+
+  async function disconnect() {
+    if (!provider) {
+      throw new Error("Provider not connected");
+    }
+    await (provider.provider as any).disconnect();
+    reset();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -70,8 +144,15 @@ function App() {
         {address ? (
           <>
             <div>{address}</div>
-            <button onClick={signMessage}>Authenticate</button>
-            <button onClick={transferDai}>Transfer DAI</button>
+            <div>
+              {!!CHAINS[chainId] ? CHAINS[chainId].chainName : "Unknown chain"}
+            </div>
+            <button onClick={signMessage}>{"Authenticate"}</button>
+            <button onClick={transferDai}>{"Transfer DAI"}</button>
+            <button onClick={switchChain}>
+              {chainId === 1 ? "Switch to Polygon" : "Switch to Ethereum"}
+            </button>
+            <button onClick={disconnect}>{"Disconnect"}</button>
           </>
         ) : (
           <button onClick={connect}>Connect</button>
